4602582a60a2e5a98fe8f9976b44da7417f09acd
beccabay <rbay@seattleu.edu>
2020-01-16 19:21:09 -0800

    made mini app

--- 5-mini-app/index.js
+++ 5-mini-app/index.js
@@ -10,3 +10,14 @@
 // get an integer using getargs
 let getargs = require("../modules/getargs/index.js");
 let input = getargs.getIntegerArg();
+
+if (isNaN(input) || !Number.isInteger(input)) {
+    console.log("usuage: node 5-mini-app <integers>");
+    } else {
+      let days = (input / 24).toFixed();
+      let hours = input % 24;
+      let dys = (days == 1) ? "day" : "days";
+      let hrs = (hours ==1) ? "hour" : "hours";
+      console.log(`${input} hours is ${days} ${dys} ${hours} ${hrs} `)
+    }
+    
